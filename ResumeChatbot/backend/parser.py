6dc5e6a63df77214d8a4154f260442efe767c86b
Manu Tyagi <manutyagi500@gmail.com>
2025-05-15 22:58:57 +0530

    Update parser.py

--- ResumeChatbot/backend/parser.py
+++ ResumeChatbot/backend/parser.py
@@ -1,6 +1,6 @@
 import pdfminer.high_level
 
 def extract_text_from_pdf(pdf_path):
-    """Extracts text from a resume PDF."""
+    """ Text Extraction """
     with open(pdf_path, "rb") as file:
         return pdfminer.high_level.extract_text(file)
